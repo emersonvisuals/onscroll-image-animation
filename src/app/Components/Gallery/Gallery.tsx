83cdb6ee7f73cd17ca02f665400c4c30bd830140
Thomas Emerson <emersonvisuals@gmail.com>
2024-02-05 20:55:51 +1100

    update

--- src/app/Components/Gallery/Gallery.tsx
+++ src/app/Components/Gallery/Gallery.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import styles from "./Gallery.module.css";
-import { motion } from "framer-motion";
 import CustomImage from "../CustomImage/CustomImage";
 import ImageOne from "../../../../public/Images/one.png";
 import ImageTwo from "../../../../public/Images/two.png";
@@ -9,6 +8,7 @@ import ImageThree from "../../../../public/Images/three.png";
 import ImageFour from "../../../../public/Images/four.png";
 import ImageFive from "../../../../public/Images/five.png";
 import ImageSix from "../../../../public/Images/six.png";
+import { motion } from "framer-motion";
 
 const Gallery = () => {
     return (
@@ -16,32 +16,32 @@ const Gallery = () => {
             <div className={styles.gallery}>
                 <motion.div
                     className={styles.imageContainer}
-                    initial={{ opacity: 0, x: 100 }}
-                    whileInView={{ opacity: 1, x: 0 }}
+                    style={{ justifyContent: "flex-start" }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{
+                        duration: 0.75,
+                        delay: 0.25,
+                        ease: "easeOut",
+                    }}
+                    viewport={{ once: true }}
+                >
+                    <CustomImage
+                        imageSource={ImageOne}
+                        imageStyle={{ width: "85%" }}
+                    />
+                </motion.div>
+                <motion.div
+                    className={styles.imageContainer}
+                    style={{ justifyContent: "flex-start" }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
                     transition={{
                         duration: 0.75,
                         delay: 0.5,
                         ease: "easeOut",
                     }}
                     viewport={{ once: true }}
-                    style={{ justifyContent: "flex-start" }}
-                >
-                    <CustomImage
-                        imageSource={ImageOne}
-                        imageStyle={{ width: "95%" }}
-                    />
-                </motion.div>
-                <motion.div
-                    className={styles.imageContainer}
-                    initial={{ opacity: 0, x: 100 }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{
-                        duration: 0.75,
-                        delay: 0.75,
-                        ease: "easeOut",
-                    }}
-                    viewport={{ once: true }}
-                    style={{ justifyContent: "flex-start" }}
                 >
                     <CustomImage
                         imageSource={ImageTwo}
@@ -50,15 +50,14 @@ const Gallery = () => {
                 </motion.div>
                 <motion.div
                     className={styles.imageContainer}
-                    initial={{ opacity: 0, x: 100 }}
-                    whileInView={{ opacity: 1, x: 0 }}
+                    style={{ justifyContent: "flex-end" }}
+                    initial={{ x: 100 }}
+                    whileInView={{ x: 0 }}
                     transition={{
                         duration: 0.75,
-                        delay: 0.5,
+                        delay: 0.25,
                         ease: "easeOut",
                     }}
-                    viewport={{ once: true }}
-                    style={{ justifyContent: "flex-end" }}
                 >
                     <CustomImage
                         imageSource={ImageThree}
@@ -67,15 +66,14 @@ const Gallery = () => {
                 </motion.div>
                 <motion.div
                     className={styles.imageContainer}
-                    initial={{ opacity: 0, x: 100 }}
-                    whileInView={{ opacity: 1, x: 0 }}
+                    style={{ justifyContent: "flex-start" }}
+                    initial={{ y: 100, x: 100 }}
+                    whileInView={{ y: 0, x: 0 }}
                     transition={{
                         duration: 0.75,
-                        delay: 0.75,
+                        delay: 0.5,
                         ease: "easeOut",
                     }}
-                    viewport={{ once: true }}
-                    style={{ justifyContent: "flex-start" }}
                 >
                     <CustomImage
                         imageSource={ImageFour}
@@ -84,32 +82,30 @@ const Gallery = () => {
                 </motion.div>
                 <motion.div
                     className={styles.imageContainer}
-                    initial={{ opacity: 0, x: 100 }}
-                    whileInView={{ opacity: 1, x: 0 }}
+                    style={{ justifyContent: "flex-start" }}
+                    initial={{ x: 100, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 100 }}
                     transition={{
                         duration: 0.75,
                         delay: 0.5,
                         ease: "easeOut",
                     }}
-                    viewport={{ once: true }}
-                    style={{ justifyContent: "flex-start" }}
                 >
                     <CustomImage
                         imageSource={ImageFive}
-                        imageStyle={{ width: "65%" }}
+                        imageStyle={{ width: "75%" }}
                     />
                 </motion.div>
                 <motion.div
-                    initial={{ opacity: 0, x: 100 }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{
-                        duration: 0.75,
-                        delay: 0.75,
-                        ease: "easeOut",
-                    }}
-                    viewport={{ once: true }}
                     className={styles.imageContainer}
                     style={{ justifyContent: "flex-start" }}
+                    initial={{ x: 100, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 100 }}
+                    transition={{
+                        duration: 0.75,
+                        delay: 0.25,
+                        ease: "easeOut",
+                    }}
                 >
                     <CustomImage
                         imageSource={ImageSix}
